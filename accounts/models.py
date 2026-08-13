0437349f9698658e40f7767fa5bcd23102a84339
Pankaj2002Chaudhary <https://github.com/Pankaj2002Chaudhary/Placement-Outreach-Automation-System-POAS-.git>
2026-08-13 23:54:52 +0530

    User Model

---
+++ accounts/models.py
@@ -0,0 +1,85 @@
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("role", User.Role.ADMIN)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(
+            email,
+            password,
+            **extra_fields
+        )
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        LIBRARIAN = "LIBRARIAN", "Librarian"
+        MEMBER = "MEMBER", "Member"
+
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+
+    email = models.EmailField(unique=True)
+
+    contact_number = models.CharField(
+        max_length=15,
+        unique=True
+    )
+
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.MEMBER
+    )
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+    REQUIRED_FIELDS = [
+        "first_name",
+        "last_name"
+    ]
+
+    def __str__(self):
+        return self.email
